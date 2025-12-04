67aa81a4ddd3502c4a722064a16cfecaf0f0d48a
a01613555-cmd <a01613555@tec.mx>
2025-12-04 08:05:50 -0600

    Temperatura.py

--- Temperatura.py
+++ Temperatura.py
@@ -1,6 +1,8 @@
-import pandas as pd
 import numpy as np
-
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.tree import DecisionTreeClassifier
 
 
 st.write(''' # Predicción de temperatura ''')
